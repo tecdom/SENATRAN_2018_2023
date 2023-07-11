571297f7ac80cd38118efd4a9880b43536c3aac4
tecdom <tecdom@gmail.com>
2023-07-11 17:52:22 -0300

    fim doa di

---
+++ dados_senatran.py
@@ -0,0 +1,7 @@
+# %% importando libs
+import pandas as pd
+import numpy as np
+
+# %% Abrindo arquivo de dados
+df=pd.read_csv('./renaest_dabertos_20230613/Acidentes_DadosAbertos_20230613.csv')
+df.head()
